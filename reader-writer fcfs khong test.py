cbad37dac01e5b00eef6709b438afedfe93c2605
Hang-2211 <hang.nm2416792@sis.hust.edu.vn>
2026-05-22 14:57:40 +0700

    Update reader-writer fcfs khong test.py

--- reader-writer fcfs khong test.py
+++ reader-writer fcfs khong test.py
@@ -7,64 +7,68 @@ import random
 # service_queue: Đảm bảo thứ tự phục vụ (ai đến trước vào hàng đợi trước)
 service_queue = threading.Semaphore(value=1)
 # resource: Bảo vệ tài nguyên chung, chỉ cho phép 1 Writer hoặc n Readers
-resource = threading.Semaphore(value=1)
-# read_count_lock: Bảo vệ biến read_count khi nhiều Reader cùng cập nhật
-read_count_lock = threading.Semaphore(value=1)
+# khởi tạo các semaphore
+# đảm bảo thứ tự phục vụ (ai đến trước vào hàng đợi trước)
+service_queue = threading.Semaphore(1)
+# bảo vệ tài nguyên chung, chỉ cho phép 1 writer hoặc n readers
+resource = threading.Semaphore(1)
+# bảo vệ biến read_count khi nhiều reader cùng đọc
+read_m = threading.Semaphore(1)
 
 read_count = 0
-
-shared_data=0
-print_lock=threading.Lock()
+shared_data = 0
+print_lock = threading.Lock()
 
 def reader(id):
-    global read_count,shared_data
+    global read_count, shared_data
     
     with print_lock:
       print(f" Reader {id} đã đến và chờ được đọc.")
     
-    # GIAI ĐOẠN VÀO (Entry Section)
-    service_queue.acquire()           # Chờ đến lượt phục vụ
-    read_count_lock.acquire()         # Khóa để cập nhật read_count
+    # entry
+    service_queue.acquire()           # chờ đến lượt phục vụ
+    read_m.acquire()         # khóa để cập nhật read_count
     
     read_count += 1
-    if read_count == 1:               # Nếu là Reader đầu tiên
-        resource.acquire()            # Khóa tài nguyên không cho Writer vào
+    if read_count == 1:               # nếu là reader đầu tiên
+        resource.acquire()            # khóa tài nguyên không cho writer vào
         
-    read_count_lock.release()
-    service_queue.release()           # Cho phép người tiếp theo trong hàng đợi vào
+    read_m.release()
+    service_queue.release()           # cho phép người tiếp theo trong hàng đợi vào
     
-    # GIAI ĐOẠN ĐỌC (Critical Section)
+    # critical
     with print_lock:
         print(f" Reader {id} đang đọc dữ liệu {shared_data} (Tổng số người đang đọc: {read_count})")
-    time.sleep(random.uniform(1, 2))  # Mô phỏng thời gian đọc
+    time.sleep(random.uniform(1, 2))  # mô phỏng thời gian đọc
     with print_lock:
         print(f" Reader {id} đã đọc xong")
-    # GIAI ĐOẠN RA (Exit Section)
-    read_count_lock.acquire()
+        
+    # exit
+    read_m.acquire()
     read_count -= 1
-    if read_count == 0:               # Nếu là Reader cuối cùng rời đi
+    if read_count == 0:               # nếu là reader cuối cùng rời đi
         with print_lock:
-            print(f"Reader {id} mở khóa")               
-        resource.release()            # Giải phóng tài nguyên cho Writer
-    read_count_lock.release()
+            print(f"Reader {id} mở khóa")            
+        resource.release()            # giải phóng tài nguyên cho writer
+    read_m.release()
 
 def writer(id):
     global shared_data
     with print_lock:
         print (f" Writer {id} đã đến và chuẩn bị viết")
     
-    # GIAI ĐOẠN VÀO (Entry Section)
-    service_queue.acquire()           # Chờ đến lượt phục vụ
-    resource.acquire()                # Chờ lấy quyền ghi độc quyền
-    service_queue.release()           # Cho phép người tiếp theo trong hàng đợi vào
+    # entry 
+    service_queue.acquire()           # chờ đến lượt phục vụ
+    resource.acquire()                # chờ lấy quyền ghi độc quyền
+    service_queue.release()           # cho phép người tiếp theo trong hàng đợi vào
     
-    # GIAI ĐOẠN GHI (Critical Section)
-    shared_data+=10
+    # critical 
+    shared_data += 10 
     with print_lock:
         print (f" Writer {id} đang ghi dữ liệu mới: {shared_data}")
-    time.sleep(random.uniform(1, 2))  # Mô phỏng thời gian ghi
+    time.sleep(random.uniform(1, 2))  # mô phỏng thời gian ghi
     
-    # GIAI ĐOẠN RA (Exit Section)
+    # exit
     with print_lock:
         print(f" Writer {id} đã ghi xong")
     resource.release()
